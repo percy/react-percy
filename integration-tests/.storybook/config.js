8f6e1153ab8fa64bf9e658d4c4069af030a89939
Tim Haines <tmhaines@gmail.com>
2017-05-31 21:11:55 -0700

    Add examples of how to freeze time and seed faker

--- integration-tests/.storybook/config.js
+++ integration-tests/.storybook/config.js
@@ -1,5 +1,15 @@
 import { configure, getStorybook } from '@kadira/storybook';
 
+import faker from 'faker';
+import timemachine from 'timemachine';
+
+// Seed faker so it generates deterministic fake data
+faker.seed(123);
+// Use timemachine to freeze the date to 2015
+timemachine.config({
+  dateString: 'October 21, 2015 13:12:59'
+});
+
 function loadStories() {
   require('../stories/index.js');
   // You can require as many stories as you need.
--- integration-tests/stories/index.js
+++ integration-tests/stories/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
+import faker from 'faker';
 
 storiesOf('Button', module)
   .add('with text', () => (
@@ -16,3 +17,33 @@ storiesOf('Text', module)
   .add('blue text using static css in sub_dir', () => (
       <p className="blue">Hi there! This text should be blue.</p>
   ));
+
+storiesOf('Time', module)
+    .add('Show the current date', () => (
+        <div>
+            <p>The current date should be frozen to 2015 thanks to timemachine.</p>
+            <p>See .storybook/config.js or&nbsp;
+            <a href="https://www.npmjs.com/package/faker#setting-a-randomness-seed">
+              faker&apos;s docs
+            </a>
+            &nbsp;for how it&apos;s configured.</p>
+            <p>The current date is: {new Date().toLocaleDateString()}</p>
+        </div>
+    ));
+
+const name = faker.name.findName();
+const email = faker.internet.email();
+
+storiesOf('Faker', module)
+    .add('Show a fake name and email', () => (
+        <div>
+            <p>The fake data should be the same thanks to faker&apos;s seed.</p>
+            <p>See .storybook/config.js or&nbsp;
+              <a href="https://www.npmjs.com/package/timemachine#config">
+                timemachine&apos;s docs
+              </a>
+              &nbsp;for how it&apos;s configured.</p>
+            <p>The name is: {name}</p>
+            <p>The email is: {email}</p>
+        </div>
+    ));
