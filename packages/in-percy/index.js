3f68bd6227c72791643efd03947869b41cdf7a76
Tim Haines <tmhaines@gmail.com>
2017-06-02 10:35:24 -0700

    Simplify inPercy

--- packages/in-percy/index.js
+++ packages/in-percy/index.js
@@ -1,6 +1,5 @@
 // Returns true if executing in the Percy rendering environment
 export default function inPercy() {
-    if (typeof window !== 'object') return false;
-    if (!window.location || !window.location.hostname) return false;
-    return window.location.hostname.match('proxyme.percy.io');
+    const hasHostname = typeof window === 'object' && window.location && window.location.hostname;
+    return hasHostname && window.location.hostname.match('proxyme.percy.io');
 }
