c254a25070f27a50e3ba47418ea399c6022ac611
Tim Haines <tmhaines@gmail.com>
2017-06-02 10:39:47 -0700

    Add comments about timemachine

--- integration-tests/.storybook/config.js
+++ integration-tests/.storybook/config.js
@@ -1,6 +1,9 @@
 import { configure, getStorybook } from '@kadira/storybook';
 import inPercy from '@percy-io/in-percy';
 import faker from 'faker';
+
+// A warning about timemachine - it overides the current date when it's imported,
+// so be sure to call reset() if this isn't desired.
 import timemachine from 'timemachine';
 
 // This demonstrates how to run code specifically for Percy's rendering environment
@@ -9,10 +12,10 @@ if (inPercy()) {
   faker.seed(123);
   // Use timemachine to freeze the date to 2015
   timemachine.config({
-    dateString: 'October 21, 2015 13:12:59'
+    dateString: 'October 21, 2015 04:19:00'
   });
 } else {
-  timemachine.reset(); // When we're not in Percy, don't use timemachine
+  timemachine.reset(); // When we're not in Percy, don't override the current date.
 }
 
 function loadStories() {
