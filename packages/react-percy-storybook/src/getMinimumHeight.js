eeb0e159d1ff2230b759ea72d2a9d5836433064c
Tim Haines <tmhaines@gmail.com>
2017-06-03 14:22:31 -0700

    Fix typo in error message

--- packages/react-percy-storybook/src/getMinimumHeight.js
+++ packages/react-percy-storybook/src/getMinimumHeight.js
@@ -1,7 +1,7 @@
 export default function getMinimumHeight(minimumHeightString) {
     if (!(/^[0-9]*$/.test(minimumHeightString))) {
         throw new Error(
-            `Minimum Height must be an integers. Received: ${minimumHeightString}`,
+            `Minimum Height must be an integer. Received: ${minimumHeightString}`,
         );
     }
 
