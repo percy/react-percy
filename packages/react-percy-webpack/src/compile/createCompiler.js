ecf5e6ce11cec303efc8230f292da5b7649c1dae
Thomas Huston <thomas@huston.io>
2017-08-19 17:49:07 -0400

    Output webpacked files to static subdirectory instead of root.

--- packages/react-percy-webpack/src/compile/createCompiler.js
+++ packages/react-percy-webpack/src/compile/createCompiler.js
@@ -1,14 +1,16 @@
 import MemoryOutputPlugin from './MemoryOutputPlugin';
 import merge from 'webpack-merge';
+import path from 'path';
 import webpack from 'webpack';
 
 export default function createCompiler(percyConfig, webpackConfig) {
   return webpack(
     merge(webpackConfig, {
       output: {
-        path: percyConfig.rootDir,
+        path: path.join(percyConfig.rootDir, 'static'),
+        publicPath: '/static/',
       },
-      plugins: [new MemoryOutputPlugin()],
+      plugins: [new MemoryOutputPlugin('/static/')],
     }),
   );
 }
