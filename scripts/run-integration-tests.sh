5b242d4409d53d994d9f89fcbbe309943b6b82ee
Thomas Huston <thomashuston@users.noreply.github.com>
2017-09-09 13:35:13 -0400

    Upgrade Yarn (#76)
    * Upgrade to yarn 1 in travis and stop linking.
    
    * Just use webpack 3.

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -9,22 +9,11 @@ error() {
   exit 1
 }
 
-link () {
-  pushd ../../packages/$1
-  yarn link
-  popd
-  yarn link @percy-io/$1
-}
-
 if [ "$SUITE" = "react-percy" ]; then
   cd integration-tests/react-percy
-  link react-percy
-  link react-percy-webpack
   DEBUG="react-percy:*" yarn percy -- --color
 elif [ "$SUITE" = "create-react-app" ]; then
   cd integration-tests/create-react-app
-  link react-percy
-  link react-percy-webpack
   DEBUG="react-percy:*" yarn percy -- --color
 else
   cat <<EOF
