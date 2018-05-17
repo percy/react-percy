2904760d673aa48dfa4163971ef0b74c7276a89d
Tim Haines <tmhaines@gmail.com>
2018-05-17 16:47:32 -0700

    Make code more readible

--- packages/react-percy-api-client/src/resources/makeRootResource.js
+++ packages/react-percy-api-client/src/resources/makeRootResource.js
@@ -1,7 +1,8 @@
 import slugify from 'slugify';
 
 export default function makeRootResource(percyClient, name, html, encodedResourceParams) {
-  let resourceUrl = `/${slugify(name, { remove: /[$*_+~.()'"!\-:@]/g }).toLowerCase()}.html`;
+  let slugified_name = slugify(name, { remove: /[$*_+~.()'"!\-:@]/g });
+  let resourceUrl = `/${slugified_name.toLowerCase()}.html`;
   if (encodedResourceParams) {
     resourceUrl = `${resourceUrl}?${encodedResourceParams}`;
   }
