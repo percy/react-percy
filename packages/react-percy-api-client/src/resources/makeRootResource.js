4b5bfc74c62d7c7b5e2ad85a683485a4539cbdf0
Tim Haines <tmhaines@gmail.com>
2018-05-17 16:55:55 -0700

    When slugifying names to create urls, strip special characters (#92)

--- packages/react-percy-api-client/src/resources/makeRootResource.js
+++ packages/react-percy-api-client/src/resources/makeRootResource.js
@@ -1,7 +1,8 @@
 import slugify from 'slugify';
 
 export default function makeRootResource(percyClient, name, html, encodedResourceParams) {
-  let resourceUrl = `/${slugify(name).toLowerCase()}.html`;
+  let slugifiedName = slugify(name, { remove: /[$*_+~.()'"!\-:@]/g });
+  let resourceUrl = `/${slugifiedName.toLowerCase()}.html`;
   if (encodedResourceParams) {
     resourceUrl = `${resourceUrl}?${encodedResourceParams}`;
   }
